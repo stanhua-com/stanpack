b5247dd53734c4712f24c93b4c717b43b9cc6b21
zqh <qunhua.zhou@audaque.com>
2020-08-09 10:23:19 +0800

    Create a folder under the parent directory

--- bin/stanpack.js
+++ bin/stanpack.js
@@ -2,7 +2,7 @@
 
 const program = require('commander')
 const pkg = require('../package.json')
-const serverLib = require('../lib/server')
+const serveLib = require('../lib/serve')
 const wxLib = require('../lib/wx')
 const antLib = require('../lib/ant')
 const pageLib = require('../lib/page')
@@ -11,15 +11,15 @@ const lineLib = require('../lib/line')
 program
   .version(pkg.version)
   .usage('<command> [options]')
-  .option('-s, --server [fileDir]', 'Server project packaging')
+  .option('-s, --serve [fileDir]', 'serve project packaging')
   .option('-w, --wx [mode] [fileDir] [dataFilePath]', 'Weixin project packaging')
   .option('-a, --ant [mode] [fileDir] [dataFilePath]', 'AntApp project packaging')
   .option('-p, --page', 'Project page list')
   .option('-l, --line [ext]', 'Statistics code line number')
   .parse(process.argv)
 
-if (program.server) {
-  serverLib(process.argv)
+if (program.serve) {
+  serveLib(process.argv)
 }
 if (program.wx) {
   wxLib(process.argv)
